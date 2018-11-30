232d58c942608535e0abd4b49bd37ffae081b927
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-11-30 16:52:28 +0100

    Adding some fixes for entry[0].content

--- spluix/spluix.sh
+++ spluix/spluix.sh
@@ -111,7 +111,7 @@ server() {
 	len=${#params[@]}
 	cache=$( refresh_cache "services" "server" "${params[@]:0:${len}-1}" )
 	if [[ ${?} == 0 ]]; then
-	    if [[ ${params[-1]} =~ (average_KBps) }}; then
+	    if [[ ${params[-1]} =~ (average_KBps) ]]; then
 		prop="entry[0].content.${params[-1]}"
 	    fi
 	    res=`jq -r ".${prop:-${params[-1]}}" ${cache} 2>/dev/null`
